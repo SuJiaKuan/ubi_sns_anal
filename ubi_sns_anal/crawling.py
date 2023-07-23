7ee0d11c9b993cf32a58d225293760db67d6e362
SuJiaKuan <jk.su@ingenta.ai>
2023-07-23 13:52:26 +0800

    Add an optional argument for crawl_fb_posts()

--- ubi_sns_anal/crawling.py
+++ ubi_sns_anal/crawling.py
@@ -21,14 +21,16 @@ def crawl_fb_posts(
     output_path,
     pages=99999,
     timeout=3000,
+    extra=False,
 ):
+    extra_str = "" if extra else "--no-extra-requests"
     cmd = \
         f"facebook-scraper " \
         f"--filename {output_path} " \
         f"--pages {pages} " \
-        f"--no-extra-requests " \
         f"--timeout {timeout} " \
         f"--cookies {cookies_path} " \
+        f"{extra_str} " \
         f"{fb_id}"
 
     return os.system(cmd)
