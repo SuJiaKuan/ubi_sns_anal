516045c054f884508cf393b715a502a588719261
SuJiaKuan <jk.su@ingenta.ai>
2023-08-12 11:33:15 +0800

    Support multiple topic filtering for analyzing

--- analyze_articles.py
+++ analyze_articles.py
@@ -9,23 +9,47 @@ from ubi_sns_anal.const import COLUMN_NAME
 from ubi_sns_anal.io import mkdir_p
 
 
-KEYWORDS = (
-    # General for 普發
-    "普發現金",
-    "現金普發",
-    "全民普發",
-    # Discussion for 紓困 4.0 in 2021
-    "普發+紓困",
-    "普發+10000",
-    "普發+10,000",
-    "普發+1萬",
-    "普發+一萬",
-    # 普發 6,000 元 in 2023
-    "普發+6000",
-    "普發+6,000",
-    "普發+6千",
-    "普發+六千",
-)
+TOPIC_KEYWORDS_MAPPING = {
+    "cash_handout": (
+        # General for 普發
+        "普發現金",
+        "現金普發",
+        "全民普發",
+        # Discussion for 紓困 4.0 in 2021
+        "普發+紓困",
+        "普發+10000",
+        "普發+10,000",
+        "普發+1萬",
+        "普發+一萬",
+        # 普發 6,000 元 in 2023
+        "普發+6000",
+        "普發+6,000",
+        "普發+6千",
+        "普發+六千",
+    ),
+    "voucher": (
+        "消費券",
+        "振興券",
+        "三倍券",
+        "五倍券",
+        "熊好券",
+        "藝文振興券",
+        "藝FUN券",
+        "文化成年禮金",
+        "農遊券",
+        "動滋券",
+        "客庄券",
+        "國旅券",
+        "i原券",
+        "好食券",
+        "地方創生券",
+    ),
+}
+TOPIC_KEYWORDS_MAPPING["all"] = [
+    keyword
+    for keywords in TOPIC_KEYWORDS_MAPPING.values()
+    for keyword in keywords
+]
 
 
 def parse_args():
@@ -51,11 +75,11 @@ def parse_args():
     return args
 
 
-def match_article(text):
+def match_article(text, keywords):
     text = text.replace(" ", "")
 
     matched_keywords = []
-    for keyword in KEYWORDS:
+    for keyword in keywords:
         sub_keywords = keyword.split("+")
         matched = all([sub_keyword in text for sub_keyword in sub_keywords])
         if matched:
@@ -64,13 +88,13 @@ def match_article(text):
     return matched_keywords
 
 
-def filter_target_articles(src_path):
+def filter_target_articles(src_path, keywords):
     uid = os.path.splitext(os.path.basename(src_path))[0]
     df = pd.read_csv(src_path, dtype=str, keep_default_na=False)
 
     articles = []
     for _, info in df.iterrows():
-        matched_keywords = match_article(info[COLUMN_NAME.TEXT])
+        matched_keywords = match_article(info[COLUMN_NAME.TEXT], keywords)
         if matched_keywords:
             articles.append({
                 COLUMN_NAME.UID: uid,
@@ -82,10 +106,10 @@ def filter_target_articles(src_path):
     return articles
 
 
-def gather_target_articles(src_paths):
+def gather_target_articles(src_paths, keywords):
     articles = []
     for src_path in tqdm(src_paths):
-        articles += filter_target_articles(src_path)
+        articles += filter_target_articles(src_path, keywords)
 
     df = pd.DataFrame(articles)
     df = df.sort_values(COLUMN_NAME.TIME)
@@ -98,10 +122,11 @@ def main(args):
 
     src_paths = glob(os.path.join(args.data, "*.csv"))
 
-    print("Gathering target articles...")
-    df = gather_target_articles(src_paths)
+    for topic, keywords in TOPIC_KEYWORDS_MAPPING.items():
+        print(f"Gathering target articles for {topic}")
+        df = gather_target_articles(src_paths, keywords)
 
-    df.to_csv(os.path.join(args.output, "analysis_results.csv"), index=False)
+        df.to_csv(os.path.join(args.output, f"{topic}.csv"), index=False)
 
     print(f"Results saved in {args.output}")
 
